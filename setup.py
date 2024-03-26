3161ce4c905a0f7c55ce5b49126616945319a09f
Samuel Chan <samuel@Samuels-MacBook-Air.local>
2024-03-26 17:33:09 +0800

    Added project description to Pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='uuid_shortener',
-    version='0.1.1',
+    version='0.1.3',
     description='A utility to shorten UUIDs using base62 encoding.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Sammeow Chan',
     author_email='1010hk@gmail.com',
-    url='https://github.com/sammeowww/py_uuid_shortener',  # Git repository URL
+    url='https://github.com/sammeowww/py_uuid_shortener',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     install_requires=[],
